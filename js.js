8bac90b8605043a8a3019d2c0b1dbe7f8b83c0d0
Jamel Ali <115441820+raizel42@users.noreply.github.com>
2023-09-04 17:23:12 +0800

    Clean javascript

--- js.js
+++ js.js
@@ -1,16 +1,15 @@
-function makeActive(){
-    var element = document.getElementById("text");
-    var see1 = document.getElementById("text2");
-    var see2 = document.getElementById("text3");
-    element.classList.add("active");
-    see1.classList.remove("active");
-    see2.classList.add("active");
-}
-function removeActive(){
-    var element = document.getElementById("text");
-    var see1 = document.getElementById("text2");
-    var see2 = document.getElementById("text3");
-    element.classList.remove("active");
-    see1.classList.add("active");
-    see2.classList.remove("active");
-}
+var element = document.getElementById("text");
+var see1 = document.getElementById("text2");
+var see3 = document.getElementById("see3");
+
+see1.addEventListener('click', function() {
+    // Toggle the "active" class on the sidebar to show/hide it
+    element.classList.toggle('active');
+
+    // If the element is active the content in See "More" will be replace in "Less"
+    if(element.classList.contains('active')){
+        see3.textContent = "Less";
+    } else {
+        see3.textContent = "More";
+    }
+}); 
